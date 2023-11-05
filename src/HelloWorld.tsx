77cd72b8876f50437966a0f3590920ecf4a93270
vlev <matiasvlevi@gmail.com>
2023-11-05 18:25:41 -0500

    init

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,16 @@
+/**
+* Node's Name
+*/
+export const Name = "HelloWorld";
+
+/**
+* Node's React Component
+*/
+export function Content({}) {
+    return <h1>HelloWorld</h1>;
+}
+
+/**
+* Node's Ports
+*/
+export const ports = [];
